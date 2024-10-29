a5ec40598b779c7fab4639efe93340687c0fe01c
potapoff <potapoff@Lev-MacBook-Air.local>
2024-10-29 18:22:36 +0100

    sdf

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student("Female", 18);
+        Employee employee = new Employee("Male", 30);
+
+
+        student.getInfo();
+        employee.getInfo();
+    }
+}
